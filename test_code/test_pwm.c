9fe6eca4263e07141912fa0c80395d2b598d1508
Vincent Klyverts Tofterup <vincenttofterup@gmail.com>
2018-02-07 14:35:57 +0100

    initial test code for servo/parachute control

---
+++ test_code/test_pwm.c
@@ -0,0 +1,41 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+int main (void)
+{
+  int l ;
+  printf ("PWM test program\n") ;
+
+  //using wiringPi Pin Number
+  int pin = 7;
+  if (wiringPiSetup () == -1)
+    exit (1) ;
+
+  /*
+  //using Physical Pin Number
+  int pin = 38;
+  if (wiringPiSetupPhys() == -1)
+    exit (1) ;
+  */
+
+  /*
+  //using BCM Pin Number
+  int pin = 5;
+  if (wiringPiSetupGpio() == -1)
+    exit (1);                   
+  */
+
+  pinMode (pin, PWM_OUTPUT);
+  for (;;) {
+    for (l = 0 ; l < 1024 ; ++l) {
+      pwmWrite (pin, l) ;
+      delay (1) ;
+    }
+    for (l = 1023 ; l >= 0 ; --l) {
+      pwmWrite (pin, l) ;
+      delay (1) ;
+    }
+  }
+  return 0 ;
+}
