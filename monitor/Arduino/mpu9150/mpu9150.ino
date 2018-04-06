4e3b6a3af1595e6421e5f62610f86d010b6d219d
Vincent Klyverts Tofterup <vincenttofterup@gmail.com>
2018-04-06 12:05:38 +0200

    arduino code, transmits mag, accel and gyro data. todo: add selftesting and possible other calibrations

--- monitor/Arduino/mpu9150/mpu9150.ino
+++ monitor/Arduino/mpu9150/mpu9150.ino
@@ -3,9 +3,9 @@
 // Author: Vincent Klyverts Tofterup, vitof13@student.sdu.dk
 // SDU - Master Thesis, stud.polyt, Robotics w. Specialization in UAV Technology
 // License: BSD 3-Clause
-// 
+//
 // Originated from: https://playground.arduino.cc/Main/MPU-9150
-// 
+//
 // Reconfigured for the specifications of the parachute project
 //
 //
@@ -21,10 +21,10 @@
 //
 // The accuracy is 16-bits.
 //
-// Important datasheets for configuration: 
+// Important datasheets for configuration:
 // https://www.invensense.com/wp-content/uploads/2015/02/MPU-9150-Datasheet.pdf
 // https://store.invensense.com/Datasheets/invensense/RM-MPU-9150A-00-v3.0.pdf
-// 
+//
 
 #include <Wire.h>
 #include <Servo.h>
@@ -67,52 +67,52 @@
 #define MPU9150_I2C_SLV4_REG       0x32   // R/W
 #define MPU9150_I2C_SLV4_DO        0x33   // R/W
 #define MPU9150_I2C_SLV4_CTRL      0x34   // R/W
-#define MPU9150_I2C_SLV4_DI        0x35   // R  
+#define MPU9150_I2C_SLV4_DI        0x35   // R
 #define MPU9150_I2C_MST_STATUS     0x36   // R
 
 #define MPU9150_INT_PIN_CFG        0x37   // R/W
 #define MPU9150_INT_ENABLE         0x38   // R/W
-#define MPU9150_INT_STATUS         0x3A   // R  
+#define MPU9150_INT_STATUS         0x3A   // R
 
-#define MPU9150_ACCEL_XOUT_H       0x3B   // R  
-#define MPU9150_ACCEL_XOUT_L       0x3C   // R  
-#define MPU9150_ACCEL_YOUT_H       0x3D   // R  
-#define MPU9150_ACCEL_YOUT_L       0x3E   // R  
-#define MPU9150_ACCEL_ZOUT_H       0x3F   // R  
-#define MPU9150_ACCEL_ZOUT_L       0x40   // R  
-#define MPU9150_TEMP_OUT_H         0x41   // R  
-#define MPU9150_TEMP_OUT_L         0x42   // R  
-#define MPU9150_GYRO_XOUT_H        0x43   // R  
-#define MPU9150_GYRO_XOUT_L        0x44   // R  
-#define MPU9150_GYRO_YOUT_H        0x45   // R  
-#define MPU9150_GYRO_YOUT_L        0x46   // R  
-#define MPU9150_GYRO_ZOUT_H        0x47   // R  
-#define MPU9150_GYRO_ZOUT_L        0x48   // R  
-#define MPU9150_EXT_SENS_DATA_00   0x49   // R  
-#define MPU9150_EXT_SENS_DATA_01   0x4A   // R  
-#define MPU9150_EXT_SENS_DATA_02   0x4B   // R  
-#define MPU9150_EXT_SENS_DATA_03   0x4C   // R  
-#define MPU9150_EXT_SENS_DATA_04   0x4D   // R  
-#define MPU9150_EXT_SENS_DATA_05   0x4E   // R  
-#define MPU9150_EXT_SENS_DATA_06   0x4F   // R  
-#define MPU9150_EXT_SENS_DATA_07   0x50   // R  
-#define MPU9150_EXT_SENS_DATA_08   0x51   // R  
-#define MPU9150_EXT_SENS_DATA_09   0x52   // R  
-#define MPU9150_EXT_SENS_DATA_10   0x53   // R  
-#define MPU9150_EXT_SENS_DATA_11   0x54   // R  
-#define MPU9150_EXT_SENS_DATA_12   0x55   // R  
-#define MPU9150_EXT_SENS_DATA_13   0x56   // R  
-#define MPU9150_EXT_SENS_DATA_14   0x57   // R  
-#define MPU9150_EXT_SENS_DATA_15   0x58   // R  
-#define MPU9150_EXT_SENS_DATA_16   0x59   // R  
-#define MPU9150_EXT_SENS_DATA_17   0x5A   // R  
-#define MPU9150_EXT_SENS_DATA_18   0x5B   // R  
-#define MPU9150_EXT_SENS_DATA_19   0x5C   // R  
-#define MPU9150_EXT_SENS_DATA_20   0x5D   // R  
-#define MPU9150_EXT_SENS_DATA_21   0x5E   // R  
-#define MPU9150_EXT_SENS_DATA_22   0x5F   // R  
-#define MPU9150_EXT_SENS_DATA_23   0x60   // R  
-#define MPU9150_MOT_DETECT_STATUS  0x61   // R  
+#define MPU9150_ACCEL_XOUT_H       0x3B   // R
+#define MPU9150_ACCEL_XOUT_L       0x3C   // R
+#define MPU9150_ACCEL_YOUT_H       0x3D   // R
+#define MPU9150_ACCEL_YOUT_L       0x3E   // R
+#define MPU9150_ACCEL_ZOUT_H       0x3F   // R
+#define MPU9150_ACCEL_ZOUT_L       0x40   // R
+#define MPU9150_TEMP_OUT_H         0x41   // R
+#define MPU9150_TEMP_OUT_L         0x42   // R
+#define MPU9150_GYRO_XOUT_H        0x43   // R
+#define MPU9150_GYRO_XOUT_L        0x44   // R
+#define MPU9150_GYRO_YOUT_H        0x45   // R
+#define MPU9150_GYRO_YOUT_L        0x46   // R
+#define MPU9150_GYRO_ZOUT_H        0x47   // R
+#define MPU9150_GYRO_ZOUT_L        0x48   // R
+#define MPU9150_EXT_SENS_DATA_00   0x49   // R
+#define MPU9150_EXT_SENS_DATA_01   0x4A   // R
+#define MPU9150_EXT_SENS_DATA_02   0x4B   // R
+#define MPU9150_EXT_SENS_DATA_03   0x4C   // R
+#define MPU9150_EXT_SENS_DATA_04   0x4D   // R
+#define MPU9150_EXT_SENS_DATA_05   0x4E   // R
+#define MPU9150_EXT_SENS_DATA_06   0x4F   // R
+#define MPU9150_EXT_SENS_DATA_07   0x50   // R
+#define MPU9150_EXT_SENS_DATA_08   0x51   // R
+#define MPU9150_EXT_SENS_DATA_09   0x52   // R
+#define MPU9150_EXT_SENS_DATA_10   0x53   // R
+#define MPU9150_EXT_SENS_DATA_11   0x54   // R
+#define MPU9150_EXT_SENS_DATA_12   0x55   // R
+#define MPU9150_EXT_SENS_DATA_13   0x56   // R
+#define MPU9150_EXT_SENS_DATA_14   0x57   // R
+#define MPU9150_EXT_SENS_DATA_15   0x58   // R
+#define MPU9150_EXT_SENS_DATA_16   0x59   // R
+#define MPU9150_EXT_SENS_DATA_17   0x5A   // R
+#define MPU9150_EXT_SENS_DATA_18   0x5B   // R
+#define MPU9150_EXT_SENS_DATA_19   0x5C   // R
+#define MPU9150_EXT_SENS_DATA_20   0x5D   // R
+#define MPU9150_EXT_SENS_DATA_21   0x5E   // R
+#define MPU9150_EXT_SENS_DATA_22   0x5F   // R
+#define MPU9150_EXT_SENS_DATA_23   0x60   // R
+#define MPU9150_MOT_DETECT_STATUS  0x61   // R
 #define MPU9150_I2C_SLV0_DO        0x63   // R/W
 #define MPU9150_I2C_SLV1_DO        0x64   // R/W
 #define MPU9150_I2C_SLV2_DO        0x65   // R/W
@@ -137,7 +137,7 @@
 #define MPU9150_CMPS_ZOUT_H        0x4F   // R
 
 
-// I2C address 0x69 could be 0x68 depends on your wiring. 
+// I2C address 0x69 could be 0x68 depends on your wiring.
 int MPU9150_I2C_ADDRESS = 0x69;
 
 
@@ -158,7 +158,7 @@ Servo myservo;
 
 
 void setup() //Initial setup and interrupt setup for free fall detection
-{        
+{
   pinMode(7,OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -168,7 +168,7 @@ void setup() //Initial setup and interrupt setup for free fall detection
   //pinMode(2,INPUT);
   //digitalWrite(2, LOW);
   myservo.attach(9);
- 
+
   // Initialize the Serial Bus for printing data.
   Serial.begin(9600);
 
@@ -187,7 +187,7 @@ void setup() //Initial setup and interrupt setup for free fall detection
   //MPU9150_writeSensor(MPU9150_FF_THR, 0x40); // Free Fall Acceleration Threshold, 32mg/LSB increment, now it is set to pure free fall gravitation
   //MPU9150_writeSensor(MPU9150_FF_DUR, 0x64); // Free Fall Duration, free fall detection in 100ms
 
-  
+
 }
 
 
@@ -197,21 +197,21 @@ void loop()
   // Formated all values as x, y, and z in order for
   // Compass, Gyro, Acceleration. The First value is
   // the temperature.
- 
+
 
   //double dT = ( (double) MPU9150_readSensor(MPU9150_TEMP_OUT_L,MPU9150_TEMP_OUT_H) + 12412.0) / 340.0;
   //Serial.print(dT);
-  //Serial.print("  ");
-  //Serial.print(MPU9150_readSensor(MPU9150_CMPS_XOUT_L,MPU9150_CMPS_XOUT_H));
-  //Serial.print(" ");
-  //Serial.print(MPU9150_readSensor(MPU9150_CMPS_YOUT_L,MPU9150_CMPS_YOUT_H));
-  //Serial.print(" ");
-  //Serial.print(MPU9150_readSensor(MPU9150_CMPS_ZOUT_L,MPU9150_CMPS_ZOUT_H));
-  //Serial.print(" ");
+  Serial.print("S ");
+  Serial.print(MPU9150_readSensor(MPU9150_CMPS_XOUT_L,MPU9150_CMPS_XOUT_H));
+  Serial.print(" ");
+  Serial.print(MPU9150_readSensor(MPU9150_CMPS_YOUT_L,MPU9150_CMPS_YOUT_H));
+  Serial.print(" ");
+  Serial.print(MPU9150_readSensor(MPU9150_CMPS_ZOUT_L,MPU9150_CMPS_ZOUT_H));
+  Serial.print(" ");
 
 
   // Only transmitting gyro and accelerometer data
-  Serial.print("S ");
+
   Serial.print(MPU9150_readSensor(MPU9150_GYRO_XOUT_L,MPU9150_GYRO_XOUT_H));
   Serial.print(" ");
   Serial.print(MPU9150_readSensor(MPU9150_GYRO_YOUT_L,MPU9150_GYRO_YOUT_H));
@@ -301,30 +301,30 @@ int MPU9150_writeSensor(int addr,int data){
 
 
 /*
- * 
- *  long acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, acc_total_vector; 
+ *
+ *  long acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, acc_total_vector;
   float angle_pitch, angle_roll;
   boolean set_gyro_angles;
   float angle_roll_acc, angle_pitch_acc;
   float angle_pitch_output, angle_roll_output;
 
-  
+
   acc_x = MPU9150_readSensor(MPU9150_ACCEL_XOUT_L,MPU9150_ACCEL_XOUT_H);
   acc_y = MPU9150_readSensor(MPU9150_ACCEL_YOUT_L,MPU9150_ACCEL_YOUT_H);
   acc_z = MPU9150_readSensor(MPU9150_ACCEL_ZOUT_L,MPU9150_ACCEL_ZOUT_H);
-  
+
   res = sqrt( sq(acc_x) + sq(acc_y) + sq(acc_z) );
 
   myservo.write(0);
-  
+
   //myservo.write(85);
-  
+
   if(res < thres){
     //Serial.print(4000);
     //Serial.print(" ");
     //digitalWrite(LED_BUILTIN, HIGH);
     //myservo.write(85);
-    
+
     //tone(7,2000);
     //delay(1000);
     //noTone(7);
@@ -335,26 +335,26 @@ int MPU9150_writeSensor(int addr,int data){
     //digitalWrite(LED_BUILTIN, LOW);
   }
 
-  
+
  * //Gyro angle calculations
   //0.0000611 = 1 / (250Hz / 65.5)
   angle_pitch += gyro_x * 0.0000611;                                   //Calculate the traveled pitch angle and add this to the angle_pitch variable
   angle_roll += gyro_y * 0.0000611;                                    //Calculate the traveled roll angle and add this to the angle_roll variable
-  
+
   //0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
 
-  
+
   angle_pitch += angle_roll * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the roll angle to the pitch angel
   angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the pitch angle to the roll angel
-  
+
   //Accelerometer angle calculations
   acc_total_vector = sqrt((acc_x*acc_x)+(acc_y*acc_y)+(acc_z*acc_z));  //Calculate the total accelerometer vector
   //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
 
-  
+
   angle_pitch_acc = asin((float)acc_y/acc_total_vector)* 57.296;       //Calculate the pitch angle
   angle_roll_acc = asin((float)acc_x/acc_total_vector)* -57.296;       //Calculate the roll angle
-  
+
   //Place the MPU-6050 spirit level and note the values in the following two lines for calibration
   angle_pitch_acc -= 0.0;                                              //Accelerometer calibration value for pitch
   angle_roll_acc -= 0.0;                                               //Accelerometer calibration value for roll
@@ -364,11 +364,11 @@ int MPU9150_writeSensor(int addr,int data){
     angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;        //Correct the drift of the gyro roll angle with the accelerometer roll angle
   }
   else{                                                                //At first start
-    angle_pitch = angle_pitch_acc;                                     //Set the gyro pitch angle equal to the accelerometer pitch angle 
-    angle_roll = angle_roll_acc;                                       //Set the gyro roll angle equal to the accelerometer roll angle 
+    angle_pitch = angle_pitch_acc;                                     //Set the gyro pitch angle equal to the accelerometer pitch angle
+    angle_roll = angle_roll_acc;                                       //Set the gyro roll angle equal to the accelerometer roll angle
     set_gyro_angles = true;                                            //Set the IMU started flag
   }
-  
+
   //To dampen the pitch and roll angles a complementary filter is used
   angle_pitch_output = angle_pitch_output * 0.9 + angle_pitch * 0.1;   //Take 90% of the output pitch value and add 10% of the raw pitch value
   angle_roll_output = angle_roll_output * 0.9 + angle_roll * 0.1;      //Take 90% of the output roll value and add 10% of the raw roll value
@@ -390,7 +390,7 @@ int MPU9150_writeSensor(int addr,int data){
   }else if(angle_pitch_output < -5.5){
     //tone(7,2000);
     //delay(500);
-    //noTone(7);   
+    //noTone(7);
   }else{
     //noTone(7);
   }
