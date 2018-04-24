c96df3886a20467be91b5e2c804a10d493a28080
Vincent Klyverts Tofterup <vitof13@student.sdu.dk>
2018-04-24 14:52:39 +0200

    moved declaration of static variables

--- monitor/definitions.h
+++ monitor/definitions.h
@@ -101,3 +101,53 @@ void pos_init(void){
 	fprintf (f, "#time_boot,lat,lon,alt,relative_alt,vx,vy,vz,heading\n");
 }
 /***************************************************************************/
+
+
+
+
+// Possible accelerometer scales (and their register bit settings) are:
+// 2 G (00), 4 G (01), 8 G (10), and 16 G  (11).
+float aRange = 2.0;       // 2G acceleration resolution (max. sensitivity)
+float Ascale = 0.0;         // Scale bit setting as 'int' --> e.g. 3 = 0b11 or 1 = 0b01
+// Possible gyro scales (and their register bit settings) are
+// 250 °/s (00), 500 °/s (01), 1000 °/s (10), and 2000 °/s  (11).
+float gRange = 250.0;     // 250°/s gyro resolution (max. sensitivity)
+float Gscale = 0.0;         // Scale bit setting as 'int' --> e.g. 3 = 0b11 or 1 = 0b01
+
+float accRes = aRange/32768.0;  // 16 bit, 2G --> 0,000061035 g = per LSB
+float gyroRes = gRange/32768.0; // 16 bit, 250 deg/sec --> = per LSB
+float compRes = 1229.0/4096.0;  // = 0,3µT per LSB --> 1 Milligauss [mG] =   0,1 Mikrotesla [µT]
+float magBias[3] = {1.8, 7.0, 8.0}; // found in arduino code, finds all these calibrations values, and does factory test in initialization
+float magCalib[3] = {1.12, 1.14, 1.18}; // found in arduino code
+
+//Definition of variables for time and integral time calculation
+uint32_t now, lastupdate, start, end;
+double dT; //need to be precise due to short time intervalls
+float a_coeff = 0.95; // Coefficient for sensor fusion (how much weight put on the angle integration --> short term trust to gyro)
+float a_CompFilter = 0.9; // Coefficient for low pass filtering the raw comp data --> e.g. 0.9 is buidling the floating average of the last 10 values(?)
+
+float pitchAcc, rollAcc, yawMag;  //Define variables for the "raw" angles calculated directly from the Acc values
+//Define variables for the angles incl. start values. Those angles are in degrees
+float pitch = 0.0;
+float roll = 0.0;
+float yaw = 0.0;
+
+//roll and pitch are needed in radians for compass tilt compensation
+float pitchRad, rollRad;
+float RadToDeg = (180.0/M_PI);
+// Declination Angle, can be found on  http://www.magnetic-declination.com/
+float declinationAngle = 2.59;
+
+float roll_offset = 4.0;
+float pitch_offset = 0.5;
+
+// thau observer varibles:
+double pos[3], old_pos[3], state[12], old_state[12], Px[12], old_Px[12], Pxdot[12], old_Pxdot[12];
+// Initial state estimate is 0.0 in each state
+for (int i = 0; i < 12; i++) {
+  Pxdot[i] = 0.0;
+  old_Pxdot[i] = 0.0;
+  Px[i] = 0.0;
+  old_Px[i] = 0.0;
+
+}
