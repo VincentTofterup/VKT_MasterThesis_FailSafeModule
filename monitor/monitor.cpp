c099f20232594e7ecf7db619729b7698df8efad7
Vincent Klyverts Tofterup <vitof13@student.sdu.dk>
2018-04-25 19:02:16 +0200

    pwm test

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -422,24 +422,24 @@ int main(){
             if (roll-roll_offset > 45.0) {
               std::cout << "roll exceed(lower) limit!" << std::endl;
               digitalWrite (CUTOFF, HIGH) ;	// Cutoff Motor system!
-              std::this_thread::sleep_for (std::chrono::milliseconds(500));
+              std::this_thread::sleep_for (std::chrono::milliseconds(1000));
               pwmWrite(PARARACHUTE,OPEN);
             }else if (roll-roll_offset < -45.0) {
               std::cout << "roll exceed(larger) limit!" << std::endl;
               digitalWrite (CUTOFF, HIGH) ;	// Cutoff Motor system!
-              std::this_thread::sleep_for (std::chrono::milliseconds(500));
+              std::this_thread::sleep_for (std::chrono::milliseconds(1000));
               pwmWrite(PARARACHUTE,OPEN);
             }
 
             if (pitch-pitch_offset > 45.0) {
               std::cout << "pitch exceed(large) limit!" << std::endl;
               digitalWrite (CUTOFF, HIGH) ;	// Cutoff Motor system!
-              std::this_thread::sleep_for (std::chrono::milliseconds(500));
+              std::this_thread::sleep_for (std::chrono::milliseconds(1000));
               pwmWrite(PARARACHUTE,OPEN);
             }else if (pitch-pitch_offset < -45.0) {
               std::cout << "pitch exceed(lower) limit!" << std::endl;
               digitalWrite (CUTOFF, HIGH) ;	// Cutoff Motor system!
-              std::this_thread::sleep_for (std::chrono::milliseconds(500));
+              std::this_thread::sleep_for (std::chrono::milliseconds(1000));
               pwmWrite(PARARACHUTE,OPEN);
             }
 
