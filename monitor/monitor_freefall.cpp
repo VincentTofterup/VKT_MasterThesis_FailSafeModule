45cf1688121f9d765bd80f7862cf8e7849c40da9
Vincent Klyverts Tofterup <vitof13@student.sdu.dk>
2018-05-16 14:08:11 +0200

    debugging

--- monitor/monitor_freefall.cpp
+++ monitor/monitor_freefall.cpp
@@ -282,10 +282,7 @@ int main(){
 
 
 
-            auto end = std::chrono::system_clock::now();
-            std::chrono::duration<double> elapsed_seconds = end-start;
-            std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-            std::cout << "Elapsed time in this loop nr:" << n << ", time: " << elapsed_seconds.count() << std::endl;
+          
 
 
 
